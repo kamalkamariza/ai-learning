5f991df39d1ed7dd1fa656854fd8f6df8d1a22c2
Ahmad Kamal <ahmad.kamal@dpodium.com>
2023-10-25 10:36:58 +0800

    modified main.py code

--- mlops/main.py
+++ mlops/main.py
@@ -6,4 +6,4 @@ result_path = os.path.join("results", "results.txt")
 os.makedirs(os.path.dirname(result_path), exist_ok=True)
 
 with open(result_path, 'w') as f:
-    f.write(f"{uuid.uuid4()} created at this point of time {datetime.now()}")
+    f.write(f"Test {uuid.uuid4()} created at this point of time {datetime.now()}")
