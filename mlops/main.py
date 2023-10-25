6504491d55de48974b31fd23216c42f49f69ce08
Ahmad Kamal <ahmad.kamal@dpodium.com>
2023-10-25 10:54:41 +0800

    debugging cml

--- mlops/main.py
+++ mlops/main.py
@@ -2,8 +2,10 @@ import os
 from datetime import datetime
 import uuid
 
+print("Running main.py")
 result_path = os.path.join("results", "results.txt")
 os.makedirs(os.path.dirname(result_path), exist_ok=True)
 
 with open(result_path, 'w') as f:
     f.write(f"Test {uuid.uuid4()} created at this point of time {datetime.now()}")
+print("Finished main.py")
